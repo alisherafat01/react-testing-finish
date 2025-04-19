a607edab374f6b93363e2773ebcbc8978b7e3a08
A.Sherafati <sherafat01@gmail.com>
2025-04-19 19:42:14 +0330

    chore: remove bug scenario from tests

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -3,9 +3,7 @@ import ProductForm from "../components/ProductForm";
 import BrowseProducts from "./BrowseProductsPage";
 
 const PlaygroundPage = () => {
-  return (
-    <ProductForm />
-  );
+  return <ProductForm />;
 };
 
 export default PlaygroundPage;
